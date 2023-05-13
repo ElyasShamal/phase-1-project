b3043aea4cf7eb529e24b0770b8620c46e2c708d
Elyas <Elyas.shamal2@gmail.com>
2023-05-13 18:08:17 -0400

    call the function and check it console

--- main.js
+++ main.js
@@ -23,9 +23,29 @@ fetch("http://localhost:3000/inventory")
  function updateSlider() {
     const slider = document.getElementById('slider');
     slider.innerHTML = '';
-
+    // create img and added to slider
     const img = document.createElement('img');
-    img.src = image[sliderIndex]
+    img.src = image[sliderIndex];
+    slider.appendChild(img);
+    //create sliderTexElem
+    const sliderTexElem = document.createElement('div');
+    sliderTexElem. classList.add('slider-text');
+    // create h3 
+    const h3 = document.createElement('h3');
+    h3.textContent = sliderText[sliderIndex].name;
+    sliderTexElem.appendChild(h3);
+    // create a paragragh
+    const p = document.createElement('p');
+    p.textContent = sliderText[sliderIndex].description;
+    sliderTexElem.appendChild(p);
+    // create a button 
+    const button = document.createElement('button');
+    button.textContent = sliderText[sliderIndex].addtocard;
+    button.id = "button-card"
+    button.classList.add('buy-btn');
+  
+    sliderTexElem.appendChild(button);
+    slider.appendChild(sliderTexElem);
 
  }  
 
