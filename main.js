340336dbd61239d4a00b5c269d8244e658fd7777
Elyas <Elyas.shamal2@gmail.com>
2023-05-14 19:35:57 -0400

    create shopping cart

--- main.js
+++ main.js
@@ -3,55 +3,60 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch("http://localhost:3000/inventory")
     .then( response => response.json())
     .then(availabelItem => availabelItem.forEach(item => createItem(item)));
-    // add addEventlistener for btn to open the form
+      // add addEventlistener for btn to open the form
     let lisButton = document.getElementById('btn');
-    lisButton.addEventListener('click', () => {
+      lisButton.addEventListener('click', () => {
     let form = document.getElementById('form')
-    form.style.visibility = 'visible';
-    // create another event for form data
+      form.style.visibility = 'visible';
+      // create another event for form data
     form.addEventListener('submit', (e) => {
-     e.preventDefault();
-     let formData = Object.fromEntries(new FormData(e.target));
-     form.style.display = 'none';
+       e.preventDefault();
+    let formData = Object.fromEntries(new FormData(e.target));
+       form.style.display = 'none';
      
-     sentItout(formData);
+       sentItout(formData);
 
     })
     })
 })
 
 function createItem(item){
-    //create a div with class name
+      //create a div with class name
     let inventory = document.createElement('div');
-    inventory.classList.add('inventory');
-    //  create image with class name
+        inventory.classList.add('inventory');
+      //  create image with class name
     let img = document.createElement('img');
-    img.classList.add('img-inventory');
-    img.src = item.image;
-    //create h3 for heading 
+        img.classList.add('img-inventory');
+        img.src = item.image;
+      //create h3 for heading 
     let h3 = document.createElement('h3');
-    h3.textContent = item.name;
-    // create a paragragh for item description
+        h3.textContent = item.name;
+      // create a paragragh for item description
     let p = document.createElement('p')
-    p.textContent = item.description;
-    // create buyButton 
+        p.textContent = item.description;
+      // create buyButton 
     let buyButton = document.createElement('button');
-    buyButton.textContent = item.addtocard;
-    buyButton.classList.add("buy-btn");
+        buyButton.textContent = item.addtocard;
+        buyButton.classList.add("buy-btn");
 
     buyButton.addEventListener('click', () => {
     let shoppingDiv = document.createElement('div');
-    shoppingDiv.classList.add('shoppingDiv')
-    // create newimage for cart
+        shoppingDiv.classList.add('shoppingDiv')
+      // create newimage for cart
     let newimage = document.createElement('img');
-    newimage.classList.add('cartImage')
-    newimage.src = item.image
+        newimage.classList.add('cartImage')
+        newimage.src = item.image
 
-    // create new H3 for item name
+      // create new H3 for item name
     let newH3 = document.createElement('h3');
-    newH3.classList.add('newH3')
-    newH3.textContent = item.name;
-    
+        newH3.classList.add('newH3')
+        newH3.textContent = item.name;
+
+      // create a new button to remove the item from cart
+
+    let remove = document.createElement('button')
+        remove.classList.add('remove');
+        remove.textContent = "Remove";
 
         
     })
@@ -114,27 +119,27 @@ fetch("http://localhost:3000/inventory")
 
  function updateSlider() {
     const slider = document.getElementById('slider');
-    slider.innerHTML = '';
-    // create img and added to slider
+          slider.innerHTML = '';
+         // create img and added to slider
     const img = document.createElement('img');
-    img.src = images[sliderIndex];
-    slider.appendChild(img);
-    //create sliderTexElem
+          img.src = images[sliderIndex];
+          slider.appendChild(img);
+         //create sliderTexElem
     const sliderTexElem = document.createElement('div');
-    sliderTexElem. classList.add('slider-text');
-    // create h3 
+          sliderTexElem. classList.add('slider-text');
+        // create h3 
     const h3 = document.createElement('h3');
-    h3.textContent = sliderText[sliderIndex].name;
-    sliderTexElem.appendChild(h3);
-    // create a paragragh
+          h3.textContent = sliderText[sliderIndex].name;
+          sliderTexElem.appendChild(h3);
+        // create a paragragh
     const p = document.createElement('p');
-    p.textContent = sliderText[sliderIndex].description;
-    sliderTexElem.appendChild(p);
-    // create a button 
+          p.textContent = sliderText[sliderIndex].description;
+          sliderTexElem.appendChild(p);
+        // create a button 
     const button = document.createElement('button');
-    button.textContent = sliderText[sliderIndex].addtocard;
-    button.id = "button-card"
-    button.classList.add('buy-btn');
+          button.textContent = sliderText[sliderIndex].addtocard;
+          button.id = "button-card"
+          button.classList.add('buy-btn');
   
     sliderTexElem.appendChild(button);
     slider.appendChild(sliderTexElem);
